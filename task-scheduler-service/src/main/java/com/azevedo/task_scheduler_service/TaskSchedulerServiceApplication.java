3171f75b843d6e4af7af9e6a6f6ee0e3aad53af2
theusazevedd <matheus_ma02@hotmail.com>
2026-04-23 19:34:51 -0300

    commit inicial com esqueleto

---
+++ task-scheduler-service/src/main/java/com/azevedo/task_scheduler_service/TaskSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.azevedo.task_scheduler_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulerServiceApplication.class, args);
+	}
+
+}
---
+++ task-scheduler-service/src/test/java/com/azevedo/task_scheduler_service/TaskSchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azevedo.task_scheduler_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
