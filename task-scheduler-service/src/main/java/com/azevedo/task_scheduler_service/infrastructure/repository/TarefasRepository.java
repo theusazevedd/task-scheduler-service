5b21d896dc8a5a6b1b2e029851536bb24cae4929
theusazevedd <matheus_ma02@hotmail.com>
2026-04-26 02:28:04 -0300

    ajusta busca de tarefas por período e melhora tratamento de exceções

--- task-scheduler-service/src/main/java/com/azevedo/task_scheduler_service/infrastructure/repository/TarefasRepository.java
+++ task-scheduler-service/src/main/java/com/azevedo/task_scheduler_service/infrastructure/repository/TarefasRepository.java
@@ -2,6 +2,7 @@ package com.azevedo.task_scheduler_service.infrastructure.repository;
 
 
 import com.azevedo.task_scheduler_service.infrastructure.entity.TarefasEntity;
+import com.azevedo.task_scheduler_service.infrastructure.enums.StatusNotificacaoEnum;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,9 @@ import java.util.List;
 @Repository
 public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
 
-    List<TarefasEntity> findByDataEventoBetween(Instant dataInicio, Instant dataFim);
+    List<TarefasEntity> findByDataEventoBetweenAndStatus(Instant dataInicio,
+                                                         Instant dataFim,
+                                                         StatusNotificacaoEnum status);
 
     List<TarefasEntity> findByEmailUsuario(String email);
 
